3eb102d6b8c533bb7ba08cffbac39cdc358aeb8c
jamesedgejr <jamesedgejr@gmail.com>
2014-07-06 15:31:51 -0400

    Tons of fun rewriting code you wrote almost 10 years ago.  Finally got PDO for matrix.php complete.
    Signed-off-by: jamesedgejr <jamesedgejr@gmail.com>

--- nessus/matrix.php
+++ nessus/matrix.php
@@ -45,8 +45,8 @@ $agency = $_POST["agency"];
 $report_name = $_POST["report_name"];
 $scan_start = $_POST["scan_start"];
 $scan_end = $_POST["scan_end"];
-$isPlugName = ($_POST["isPlugName"] == "y") ? "y" : "n";
-$isPlugFam = ($_POST["isPlugFam"] == "y") ? "y" : "n";
+$isPlugName = $_POST["isPlugName"];
+$isPlugFam = $_POST["isPlugFam"];
 $pivot = $_POST["pivot"];
 
 date_default_timezone_set('UTC');
@@ -54,6 +54,37 @@ $myDir = "/var/www/projectRF/nessus/csvfiles/";
 $myFileName = $agency . "_" . date('mdYHis') . ".csv";
 $myFile = $myDir . $myFileName;
 $fh = fopen($myFile, 'w') or die("can't open $myFile for writing.  Please check folder permissions.");
+?>
+<html>
+<head>
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>NESSUS VULNERABILITY MATRIX</title>
+<style type="text/css">
+p {font-size: 90%}
+a {text-decoration: none}
+a:hover {text-decoration: underline}
+</style>
+</head>
+<body>
+<table width="100%"><tr>
+	<td width="20%" valign="top">
+	<?php include '../main/menu.php'; ?>
+	</td>
+	<td valign="top">
+		<hr>
+<?php
+if($isPlugName != "y" && $isPlugFam != "y"){
+			echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.1";
+?>
+		<hr>
+	</td>
+</tr></table>
+</body>
+</html>
+<?php
+exit;
+}
 
 //hosts are left side with vulnerabilities across the top
 if($pivot == "left"){
@@ -85,7 +116,9 @@ if($pivot == "left"){
 	$pluginData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	//$pluginData =& $db->getAll($sql, array(), DB_FETCHMODE_ASSOC);
 	foreach($pluginData as $pD){
-		$pluginName = str_replace("&#039;","'",$pD["pluginName"]);
+		$pluginName = $pD["pluginName"];
+		$pluginName = str_replace("&#039;","'",$pluginName);
+		$pluginName = str_replace("&lt;","<",$pluginName);
 		if($isPlugName == "y"){
 			fwrite($fh, "\"$pluginName\"");
 		}
@@ -95,7 +128,9 @@ if($pivot == "left"){
 	
 	fwrite($fh, "\"\",\"\",\"\",");
 	foreach($pluginData as $pD){
-		$pluginFamily = str_replace("&#039;","'", $pD["pluginFamily"]);
+		$pluginFamily = $pD["pluginFamily"];
+		$pluginFamily = str_replace("&#039;","'", $pluginFamily);
+		$pluginFamily = str_replace("&lt;","<", $pluginFamily);
 		if($isPlugFam == "y"){
 			fwrite($fh, "\"$pluginFamily\"");
 		}
@@ -185,135 +220,133 @@ if($pivot == "left"){
 
 	if($isPlugFam == "y" && $isPlugName == "y"){
 		$spacer = "\"\",\"\",\"\",";
-	} elseif ($isPlugFam == "n" && $isPlugName == "y"){
+	} elseif ($isPlugFam != "y" && $isPlugName == "y"){
 		$spacer = "\"\",\"\",";
-	} elseif ($isPlugFam == "y" && $isPlugName == "n"){
+	} elseif ($isPlugFam == "y" && $isPlugName != "y"){
 		$spacer = "\"\",\"\",";
 	} else {
-		echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.";
+		echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.2";
 		exit;
 	}
-	$hostArray = array();
-	$sql = "SELECT DISTINCT
+
+	$host_sql = "SELECT DISTINCT
 				nessus_tags.host_name,
+				nessus_tags.ip_addr,
 				nessus_tags.fqdn,
-				nessus_tags.operating_system
+				nessus_tags.operating_system,
+				nessus_tags.netbios
 			FROM
 				nessus_tags
-			Inner Join nessus_results ON nessus_tags.tagID = nessus_results.tagID
-			Inner Join nessus_tmp_hosts ON nessus_tmp_hosts.host_name = nessus_tags.host_name
+			Inner Join nessus_results ON nessus_results.tagID = nessus_tags.tagID
 			Inner Join nessus_tmp_family ON nessus_tmp_family.pluginFamily = nessus_results.pluginFamily
-			Inner Join nessus_temp_severity ON nessus_temp_severity.severity = nessus_results.severity
+			Inner Join nessus_tmp_hosts ON nessus_tmp_hosts.host_name = nessus_tags.host_name
+			Inner Join nessus_tmp_severity ON nessus_tmp_severity.severity = nessus_results.severity
 			WHERE
-				nessus_results.agency = '$agency' AND 
-				nessus_results.report_name = '$report_name' AND
-				nessus_results.scan_start = '$scan_start' AND
-				nessus_results.scan_end = '$scan_end'
+				nessus_results.agency =  ? AND
+				nessus_results.report_name =  ? AND
+				nessus_results.scan_start =  ? AND
+				nessus_results.scan_end =  ? 
 			ORDER BY 
-				nessus_tags.host_name ASC 
+				nessus_tags.ip_addr ASC 
 			";
-	$hostData =& $db->getAll($sql, array(), DB_FETCHMODE_ASSOC);ifError($hostData);
+	$host_data = array($agency, $report_name, $scan_start, $scan_end);
+	$host_stmt = $db->prepare($host_sql);
+	$host_stmt->execute($host_data);
+	$host_row = $host_stmt->fetchAll(PDO::FETCH_ASSOC);
 	fwrite($fh, $spacer);
-	fwrite($fh, "\"Host Name\",");
-	foreach($hostData as $hD){
-		$host_name = $hD["host_name"];
-		fwrite($fh, "\"$host_name\",");
+	foreach($host_row as $hR){
+		$ip_addr = $hR["ip_addr"];
+		fwrite($fh, "\"$ip_addr\",");
 	}
 	fwrite($fh, "\n");
 	fwrite($fh, $spacer);
-	fwrite($fh, "\"FQDN\",");
-	foreach($hostData as $hD){
-		$fqdn = $hD["fqdn"];
-		fwrite($fh, "\"$fqdn\",");
+	foreach($host_row as $hR){
+		if($host_row["fqdn"] == ""){
+			$hostname = $hR["netbios"];
+		} else {
+			$hostname = $hR["fqdn"];
+		}
+		fwrite($fh, "\"$hostname\",");
 	}
 	fwrite($fh, "\n");
 	fwrite($fh, $spacer);
-	fwrite($fh, "\"Operating System\",");
-	foreach($hostData as $hD){
-		$operating_system = $hD["operating_system"];
-		fwrite($fh, "\"$operating_system\",");
-	}
-	fwrite($fh, "\n\n");
-	if($isPlugFam == "y" && $isPlugName == "y"){
-		fwrite($fh, "\"Plugin Family\",\"Plugin Name\",\"Severity\",");
-	} elseif ($isPlugFam == "n" && $isPlugName == "y"){
-		fwrite($fh, "\"Plugin Name\",\",\"Severity\",");
-	} elseif ($isPlugFam == "y" && $isPlugName == "n"){
-		fwrite($fh, "\"Plugin Family\",\"Severity\",");
-	} else {
-		echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.";
-		exit;
+	foreach($host_row as $hR){
+		$os = $hR["operating_system"];
+		$os = str_replace("Enterprise", "Ent", $os);
+		$os = str_replace("Standard", "Std", $os);
+		$os = str_replace("Service Pack", "SP", $os);
+		$os = str_replace("Microsoft", "", $os);
+		$os = str_replace("Edition", "Ed", $os);
+		$os = str_replace("(English)", "", $os);
+		fwrite($fh, "\"$os\",");
 	}
 	fwrite($fh, "\n");
-	$sql = "SELECT DISTINCT
+	$pluginID_sql = "SELECT DISTINCT
 				nessus_results.pluginID,
 				nessus_results.pluginName,
 				nessus_results.pluginFamily,
 				nessus_results.cvss_base_score,
-				nessus_results.severity
+				nessus_results.risk_factor
 			FROM
 				nessus_results
-			INNER JOIN nessus_tmp_hosts ON nessus_tmp_hosts.host_name = nessus_tags.host_name
 			INNER JOIN nessus_tmp_severity ON nessus_tmp_severity.severity = nessus_results.severity
 			INNER JOIN nessus_tmp_family ON nessus_tmp_family.pluginFamily = nessus_results.pluginFamily
 			WHERE
-				nessus_results.agency = '$agency' AND 
-				nessus_results.report_name = '$report_name' AND
-				nessus_results.scan_start = '$scan_start' AND
-				nessus_results.scan_end = '$scan_end'
-			ORDER BY $sortOrder
+				nessus_results.agency =  ? AND
+				nessus_results.report_name =  ? AND
+				nessus_results.scan_start =  ? AND
+				nessus_results.scan_end =  ? 
+			ORDER BY nessus_results.cvss_base_score DESC
 			";
-	$pluginData =& $db->getAll($sql, array(), DB_FETCHMODE_ASSOC);ifError($pluginData);
-	foreach ($pluginData as $pD){
-		$pluginID = $row["pluginID"];
-		$pluginName = $row["pluginName"];
-		$pluginFamily = $row["pluginFamily"];
+	$pluginID_data = array($agency, $report_name, $scan_start, $scan_end);
+	$pluginID_stmt = $db->prepare($pluginID_sql);
+	$pluginID_stmt->execute($pluginID_data);
+	$pluginID_row = $pluginID_stmt->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($pluginID_row as $pR){
+		$pluginID = $pR["pluginID"];
+		$pluginName = $pR["pluginName"];
+		$pluginName = str_replace("&#039;","'",$pluginName);
+		$pluginName = str_replace("&lt;","<",$pluginName);
+		$pluginFamily = $pR["pluginFamily"];
+		$pluginFamily = str_replace("&#039;","'",$pluginFamily);
+		$pluginFamily = str_replace("&lt;","<",$pluginFamily);
 		if($isPlugFam == "y" && $isPlugName == "y"){
 			fwrite($fh, "\"$pluginFamily\",\"$pluginName\",");
-		} elseif ($isPlugFam == "n" && $isPlugName == "y"){
-			fwrite($fh, "\"$pluginName\",\",");
-		} elseif ($isPlugFam == "y" && $isPlugName == "n"){
+		} elseif ($isPlugFam != "y" && $isPlugName == "y"){
+			fwrite($fh, "\"$pluginName\",");
+		} elseif ($isPlugFam == "y" && $isPlugName != "y"){
 			fwrite($fh, "\"$pluginFamily\",");
 		} else {
-			echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.";
+			echo "You need to select either Plugin Name, Plugin Family, or both.  Deselecting both of them will produce a really useless report.3";
 			exit;
 		}
-		$cvss_base_score = $row["cvss_base_score"];
-		$severity = $row["severity"];
-		if ($cvss_base_score == "10.0"){
-			$risk = "Critical";
-		} else if ($severity == "3") {
-			$risk = "High";
-		} else if ($severity == "2") {
-			$risk = "Medium";
-		} else if ($severity == "1") {
-			$risk = "Low";
-		} else if($severity == "0"){
-			$risk = "Information";
-		}	
-		fwrite($fh, "\"$risk\",\"\",");
+		$cvss_base_score = $pR["cvss_base_score"];
+		$risk_factor_letter = substr($pR["risk_factor"],0,1);
+		fwrite($fh, "\"$risk_factor_letter ($cvss_base_score)\",");
 		//we need to lookup and find if the host is vulnerable to the vulnerability
-		foreach($hostData as $hD){
-			$host_name = $hD["host_name"];
+		foreach($host_row as $hR){
+			$host_name = $hR["host_name"];
 			$lookup_sql = "SELECT DISTINCT
-						nessus_results.pluginID
+							nessus_results.pluginID
 					FROM
-						nessus_results
-					INNER JOIN nessus_tmp_hosts ON nessus_tmp_hosts.host_name = nessus_tags.host_name
-					INNER JOIN nessus_tmp_severity ON nessus_tmp_severity.severity = nessus_results.severity
-					INNER JOIN nessus_tmp_family ON nessus_tmp_family.pluginFamily = nessus_results.pluginFamily
+						nessus_tags
+					Inner Join nessus_results ON nessus_results.tagID = nessus_tags.tagID
+					Inner Join nessus_tmp_family ON nessus_tmp_family.pluginFamily = nessus_results.pluginFamily
+					Inner Join nessus_tmp_hosts ON nessus_tmp_hosts.host_name = nessus_tags.host_name
+					Inner Join nessus_tmp_severity ON nessus_tmp_severity.severity = nessus_results.severity
 					WHERE
-						nessus_tags.host_name = '$host_name' AND
-						nessus_results.agency = '$agency' AND
-						nessus_results.report_name = '$report_name' AND
-						nessus_results.pluginID = '$pluginID' AND
-						nessus_results.scan_start = '$scan_start' AND
-						nessus_results.scan_end = '$scan_end'		
+						nessus_tags.host_name = ? AND
+						nessus_results.agency = ? AND
+						nessus_results.report_name = ? AND
+						nessus_results.scan_start = ? AND
+						nessus_results.scan_end = ? AND 
+						nessus_results.pluginID = ?		
 			";
-			$lookup_results = $db->query($lookup_sql);ifError($lookup_results);
-			$num = $lookup_results->numRows();
-			if($num == 0){
-				//IF NUM = 0 THEN NESSUS DID NOT FIND THIS ip VULNERABLE TO THIS PLUGIN
+			$lookup_data = array($host_name, $agency, $report_name, $scan_start, $scan_end, $pluginID);
+			$lookup_stmt = $db->prepare($lookup_sql);
+			$lookup_stmt->execute($lookup_data);
+			$num_rows = count($lookup_stmt->fetchAll(PDO::FETCH_ASSOC));
+			if($num_rows == 0){
 				fwrite($fh, "\"\",");
 			}
 			else {
@@ -327,24 +360,8 @@ if($pivot == "left"){
 
 
 ?>
-<html>
-<head>
-  <meta content="text/html; charset=ISO-8859-1"
- http-equiv="content-type">
-  <title>NESSUS VULNERABILITY MATRIX</title>
-<style type="text/css">
-p {font-size: 90%}
-a {text-decoration: none}
-a:hover {text-decoration: underline}
-</style>
-</head>
-<body>
-<table width="100%"><tr>
-	<td width="20%" valign="top">
-	<?php include '../main/menu.php'; ?>
-	</td>
-	<td valign="top">
-		<hr>
+
+
 		<p align="center"><a href="csvfiles/<?php echo "$myFileName";?>">Click Here</a> to download the CSV file.</p>
 		<hr>
 	</td>
